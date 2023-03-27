786286c84a9247463a78041c7100dd2635fa27e1
AndresSCP <andres.contreras.p@gmail.com>
2023-03-27 11:54:57 -0300

    actualizacion

--- src/main/java/servlet/Login.java
+++ src/main/java/servlet/Login.java
@@ -37,11 +37,6 @@ public class Login extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
-        response.sendRedirect("login.jsp");
 
     }
 
